1c331977f9474e201ee53da6232ac507d752a521
rakeshm <rakeshm@PRINHYLTPAP0772.local>
2019-03-21 00:52:00 +0530

    mocklocation detection api change

--- src/android/CheckMockLocationsEnabled.java
+++ src/android/CheckMockLocationsEnabled.java
@@ -10,34 +10,53 @@ import org.json.JSONObject;
 import 	android.provider.Settings;
 import android.location.Location;
 import android.location.LocationManager;
+import android.location.LocationListener;
 
-public class CheckMockLocationsEnabled extends CordovaPlugin {
+public class CheckMockLocationsEnabled extends CordovaPlugin implements LocationListener {
 
+    private boolean mockLocationEnabled;
+    protected LocationManager locationManager;
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         /*callbackContext.success(1);
         return true;*/
 
         //if (action.equals("check")) {
-            this.check(callbackContext);
-            return true;
+            // this.check(callbackContext);
+            // return true;
         //}
         //return false;
+        if(this.mockLocationEnabled) {
+            callbackContext.success("From Mock Provider");
+        } else {
+            callbackContext.success("Real Location");
+        }
 
+        return true;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        this.mockLocationEnabled = location.isFromMockProvider();
     }
 
     private void check(CallbackContext callbackContext) {
-      try {
-            Location location = new Location(LocationManager.GPS_PROVIDER);
+    //   try {
+    //         this.locationManager = (LocationManager) callbackContext.getSystemService(LOCATION_SERVICE);
 
-        //   if (Settings.Secure.getString(this.cordova.getActivity().getContentResolver(), Settings.Secure.ALLOW_MOCK_LOCATION).equals("0")) {
-        if(location.isFromMockProvider()) {
-              callbackContext.success("From Mock Provider");
-          } else {
-              callbackContext.success("Not From Mock Provider");
-          }
-      } catch (Exception e) {
-        callbackContext.error(e.getMessage());
-      }
+    //         boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+    //         Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+
+    //     //   if (Settings.Secure.getString(this.cordova.getActivity().getContentResolver(), Settings.Secure.ALLOW_MOCK_LOCATION).equals("0")) {
+    //     if(location.isFromMockProvider()) {
+    //           callbackContext.success("From Mock Provider");
+    //       } else {
+    //           callbackContext.success("Not From Mock Provider");
+    //       }
+    //   } catch (Exception e) {
+    //     callbackContext.error(e.getMessage());
+    //   }
     }
 }
